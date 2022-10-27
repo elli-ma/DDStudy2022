a7adfe040144ca6bc3403e1522233f0edeab7bb9
elli-ma <elli9225@gmail.com>
2022-10-27 17:00:39 +0300

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext <DAL.DataContext> (options=>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql"), sql => { })
+        });
+
+        var app = builder.Build();
+
+      
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
